87c9432a85726bf46ac3027400a5e65a5c81601c
markvezina <150861107+markvezina@users.noreply.github.com>
2025-06-17 18:35:24 -0500

    changes

---
+++ backend/src/main.py
@@ -0,0 +1,10 @@
+from OTXv2 import OTXv2
+from OTXv2 import IndicatorTypes
+import os
+from dotenv import load_dotenv, dotenv_values
+
+#load api key variable
+load_dotenv()
+apiKey = os.getenv("MY_KEY")
+
+
